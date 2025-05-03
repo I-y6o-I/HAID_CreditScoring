2215c40cef9049167cb44e3fdd3ed7e8ea6b0435
|-y6o-| <mailkirill17@gmail.com>
2025-05-03 21:06:58 +0300

    feat(ml_model): added training pipeline

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,4 +1,17 @@
 from pydantic import BaseModel
 
 class PredictionRequest(BaseModel):
+    gender: int
+    days_birth: int
+    amt_incomne_total: int
+    days_employed: int
+    flag_own_car: bool
+    flag_own_realty: bool
+    code_income_type: int
+    code_education_type: int
+    code_family_status: int
+    cnt_family_members: int
+    cnt_children: int
+    code_housing_type: int
+    code_occupation_type: int
     
